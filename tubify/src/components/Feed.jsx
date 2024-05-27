a6e8001abb47816ee04ee122ce1efaf16fb78c26
emperorbj <cwaku96@gmail.com>
2024-05-27 12:48:13 -0700

    fetching from rapid api

--- tubify/src/components/Feed.jsx
+++ tubify/src/components/Feed.jsx
@@ -1,7 +1,18 @@
 import SideBar from './SideBar';
 import VideoDetails from './VideoDetails';
+import { useEffect, useState } from "react";
 
+import { fetchFromAPI } from '../utils/fetch';
 const Feed = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [videos, setVideos] = useState(null);
+    useEffect(() => {
+        setVideos(null);
+    
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+        .then((data) => setVideos(data.items))
+        }, [selectedCategory]);
     return (
         <div className=" w-full h-screen bg-blue-600">
             <div className='flex gap-4 mt-1'>
--- tubify/src/utils/fetch.js
+++ tubify/src/utils/fetch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 
-const BASE_URL = 'https://youtube-data8.p.rapidapi.com/search/'
+const BASE_URL = 'https://youtube-data8.p.rapidapi.com';
 const apiKey = import.meta.env.REACT_APP_RAPID_API_KEY
 const options = {
         method: 'GET',
@@ -20,6 +20,6 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
         const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-        
+
         return data;
 };
