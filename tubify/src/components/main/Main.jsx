61a051ec6fb815c486166d484ddf9cc48db0aa42
emperorbj <cwaku96@gmail.com>
2024-05-31 15:12:39 +0100

    styling response page

--- tubify/src/components/main/Main.jsx
+++ tubify/src/components/main/Main.jsx
@@ -26,11 +26,9 @@ const Main = () => {
                 {!showResult
                 ?
                 <>
-                </>
-                :}
                 {/* greet */}
                 <div className='my-[50px] mx-[0px] text-slate-400 font-bold p-[20px] '>
-                    <p><span className=' bg-gradient-to-r from-blue-400 to-red-400 bg-clip-text text-transparent text-[60px]'>Hello Senior Dev,</span></p>
+                    <p><span className=' bg-gradient-to-r from-blue-600 to-green-600 bg-clip-text text-transparent text-[60px]'>Hello Senior Dev,</span></p>
                     <p className='text-[40px]'>How can I help you today ?</p>
                 </div>
                 {/* cards */}
@@ -53,14 +51,43 @@ const Main = () => {
                         <img className=' absolute w-[40px] p-[5px] bg-white rounded-[20px] bottom-[10px] right-[10px]' src={assets.message_icon} alt="" />
                     </div>
                 </div>
+                </>
+                :
+                // results
+                <div className='result py-[0px] px-[5%] max-h-[70vh] overflow-y-scroll'>
+                    {/* result title */}
+                    <div className='my-[40px] mx-[0px] flex items-center gap-[20px]'>
+                        <img className='w-[40px] rounded-3xl' src={assets.user_icon} alt="" />
+                        {recentPrompt}
+                    </div>
+                    {/* result data */}
+                    <div className='flex items-start gap-[20px]'>
+                        <img src={assets.gemini_icon} alt="" />
+                        {
+                            loading?
+                            <div className='loader w-[100%] flex flex-col gap-[10px]'>
+                                <hr className=' rounded-[4px] border-none 
+                                bg-white bg-gradient-to-r from-blue-600 to-green-600
+                                loading' />
+                                <hr />
+                                <hr />
+                            </div>
+                            :
+                            <p className='text-[20px] font-medium leading-relaxed ' dangerouslySetInnerHTML={{__html:resultData}}></p>
+                        }
+                        
+                    </div>
+                </div>
+                }
+
                 <div className='absolute  mt-[80px] bottom-0 w-[100%] max-w-[900px] py-0 px-[20px] m-auto'>
                     {/* search bar */}
                     <div className=' flex items-center justify-between gap-[20px] bg-slate-100 py-[40px] px-[30px] rounded-[20px]'>
                         <input onChange={(e) => setInput(e.target.value)} value={input} className=' outline-none border-none bg-slate-100 w-[600px] text-[20px]' type="text" placeholder='Enter a prompt here' />
-                        <div className='flex gap-2'>
-                            <img width={25} src={assets.gallery_icon} alt="" />
-                            <img width={25} src={assets.mic_icon} alt="" />
-                            <img onClick={() => onSend()} width={25} src={assets.send_icon} alt="" />
+                        <div className='flex gap-4'>
+                            <img className=' cursor-pointer' width={30}  src={assets.gallery_icon} alt="" />
+                            <img className=' cursor-pointe ' width={30} src={assets.mic_icon} alt="" />
+                            <img className=' cursor-pointer' onClick={() => onSend()} width={30} src={assets.send_icon} alt="" />
                         </div>
                     </div>
                     <p className=' my-5 mt-[15px]'>
